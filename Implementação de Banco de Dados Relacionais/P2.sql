40ff3e1b9452122f4ad05a5a9eb22f2ac6ccbb34
Silas B. Ferreira <33920489+SilasPDJ@users.noreply.github.com>
2022-06-07 23:22:31 -0300

    finalizando-semestre3

---
+++ Implementação de Banco de Dados Relacionais/P2.sql
@@ -0,0 +1,10 @@
+CREATE TABLE pacientes (
+nome varchar (80) NOT NULL,
+cpf varchar (11) PRIMARY KEY NOT NULL UNIQUE,
+endereço varchar (80),
+bairro varchar (80),
+telefone varchar (30)
+);
+
+select * from pacientes where nome="Ana Maria de Oliveira Freitas" LIMIT 1;
+SET autocommit = ON;
